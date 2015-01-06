3be18ddd4747fb1cccce87ae437f744d5707a85c
James Best <jim@justjibba.net>
2015-01-06 07:39:00 +0700

    Added Foundation Kitchen Sink example and author meta

--- single.php
+++ single.php
@@ -9,22 +9,27 @@
 	 * @since       Timber 0.1
 	 */
 
-	$context = Timber::get_context();
-	$post = Timber::query_post();
+	//	For fullscreen template change to true
+	$full_screen     = false;
+	$context         = Timber::get_context();
+	$post            = Timber::query_post();
 	$context['post'] = $post;
 	$context['wp_title'] .= ' - ' . $post->title();
 	$context['comment_form'] = TimberHelper::get_comment_form();
+
 	/* Dynamic Sidebar */
 	$context['sidebar'] = Timber::get_widgets( 'Sidebar' );
 
 	if ( post_password_required( $post->ID ) ) {
 		Timber::render( 'single-password.twig', $context );
+	} elseif ( $full_screen ) {
+		Timber::render( 'single-full.twig', $context );
 	} else {
 		Timber::render( array (
-				'single-' . $post->ID . '.twig',
-				'single-' . $post->post_type . '.twig',
-				'single.twig'
-			), $context );
+			'single-' . $post->ID . '.twig',
+			'single-' . $post->post_type . '.twig',
+			'single.twig'
+		), $context );
 	}
 
 
