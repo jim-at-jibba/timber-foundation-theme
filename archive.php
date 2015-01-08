68e90d8dc21428d8c86649d1f4557c17df5f9d16
James Best <jim@justjibba.net>
2015-01-08 16:38:23 +0700

    Added Achive View and added sidebar and pagination to archive pages

--- archive.php
+++ archive.php
@@ -9,32 +9,34 @@
  *
  * Methods for TimberHelper can be found in the /functions sub-directory
  *
- * @package 	WordPress
- * @subpackage 	Underwood
- * @since 		Underwood 0.0.1
+ * @package       WordPress
+ * @subpackage    Underwood
+ * @since         Underwood 0.0.1
  */
 
-		$templates = array('archive.twig', 'index.twig');
+$templates = array( 'archive.twig', 'index.twig' );
 
-		$data = Timber::get_context();
+$context               = Timber::get_context();
+$context['pagination'] = Timber::get_pagination();
+/* Dynamic Sidebar */
+$context['sidebar'] = Timber::get_widgets( 'Sidebar' );
+$context['title']   = 'Archive';
+if ( is_day() ) {
+	$context['title'] = 'Archive: ' . get_the_date( 'D M Y' );
+} else if ( is_month() ) {
+	$context['title'] = 'Archive: ' . get_the_date( 'M Y' );
+} else if ( is_year() ) {
+	$context['title'] = 'Archive: ' . get_the_date( 'Y' );
+} else if ( is_tag() ) {
+	$context['title'] = single_tag_title( '', false );
+} else if ( is_category() ) {
+	$context['title'] = single_cat_title( '', false );
+	array_unshift( $templates, 'archive-' . get_query_var( 'cat' ) . '.twig' );
+} else if ( is_post_type_archive() ) {
+	$context['title'] = post_type_archive_title( '', false );
+	array_unshift( $templates, 'archive-' . get_post_type() . '.twig' );
+}
 
-		$data['title'] = 'Archive';
-		if (is_day()){
-			$data['title'] = 'Archive: '.get_the_date( 'D M Y' );
-		} else if (is_month()){
-			$data['title'] = 'Archive: '.get_the_date( 'M Y' );
-		} else if (is_year()){
-			$data['title'] = 'Archive: '.get_the_date( 'Y' );
-		} else if (is_tag()){
-			$data['title'] = single_tag_title('', false);
-		} else if (is_category()){
-			$data['title'] = single_cat_title('', false);
-			array_unshift($templates, 'archive-'.get_query_var('cat').'.twig');
-		} else if (is_post_type_archive()){
-			$data['title'] = post_type_archive_title('', false);
-			array_unshift($templates, 'archive-'.get_post_type().'.twig');
-		}
+$context['posts'] = Timber::get_posts();
 
-		$data['posts'] = Timber::get_posts();
-
-		Timber::render($templates, $data);
+Timber::render( $templates, $context );
