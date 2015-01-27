e7d1351c2cab935dc14de881c86eab220e13e8d7
James G. Best <jim-at-jibba@users.noreply.github.com>
2015-01-27 15:08:54 +0700

    Update 404.php

--- 404.php
+++ 404.php
@@ -4,9 +4,9 @@
  *
  * Methods for TimberHelper can be found in the /functions sub-directory
  *
- * @package  WordPress
- * @subpackage  Timber
- * @since    Timber 0.1
+ * @package  	WordPress
+ * @subpackage  Underwood
+ * @since    	Underwood 0.0.1
  */
 
 	$context = Timber::get_context();
