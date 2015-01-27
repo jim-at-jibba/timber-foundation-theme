12c20da74a39c5480c4fd4aabf9a9324c67754ac
James G. Best <jim-at-jibba@users.noreply.github.com>
2015-01-27 15:22:49 +0700

    Update images.js

--- gulp/tasks/images.js
+++ gulp/tasks/images.js
@@ -1,6 +1,7 @@
 var changed    = require('gulp-changed');
 var gulp       = require('gulp');
 var imagemin   = require('gulp-imagemin');
+var notify     = require('gulp-notify');
 
 gulp.task('images', function() {
 	var dest = 'img'; // Image Destination
@@ -9,4 +10,8 @@ gulp.task('images', function() {
 		.pipe(changed(dest)) // Ignore unchanged files
 		.pipe(imagemin()) // Optimize
 		.pipe(gulp.dest(dest));
+		.pipe(notify({
+	            title: 'Success',
+	            message: 'Your styles have compiled successfully'
+	        }))
 });
